a25ef69e2952432ffceb700ce49894ecb0034941
Made Fabian <madefabian882@gmail.com>
2026-01-09 11:17:28 +0700

    menambahkan tailwind

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>@yield('title')</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-gray-100 min-h-screen">
+    @yield('content')
+</body>
+</html>
