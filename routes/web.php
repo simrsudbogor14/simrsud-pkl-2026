e2eed45ee0cf78457ca06943b98e8c1690ae12d1
Made Fabian <madefabian882@gmail.com>
2026-01-15 08:49:15 +0700

    perbaikan route

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,80 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+// ===================
+// LOGIN (PAKAI CONTROLLER)
+// ===================
 Route::get('/', [AuthController::class, 'login']);
 Route::post('/login', [AuthController::class, 'doLogin']);
 
+
+// ===================
+// HALAMAN MENU (WAJIB LOGIN)
+// ===================
 Route::get('/menu/{nama}', function ($nama) {
+
     if (!session('login')) {
         return redirect('/');
     }
 
-    return view('menu', compact('nama'));
+    $allComments = session('comments', []);
+    $comments = $allComments[$nama] ?? [];
+
+    return view('menu', compact('nama', 'comments'));
+});
+
+
+// ===================
+// SIMPAN KOMENTAR
+// ===================
+Route::post('/comment/{nama}', function (Request $request, $nama) {
+
+    if (!session('login')) {
+        return redirect('/');
+    }
+
+    $request->validate([
+        'nama' => 'required',
+        'isi'  => 'required',
+    ]);
+
+    $allComments = session('comments', []);
+
+    if (!isset($allComments[$nama])) {
+        $allComments[$nama] = [];
+    }
+
+    $allComments[$nama][] = [
+        'nama' => $request->nama,
+        'isi'  => $request->isi,
+    ];
+
+    session(['comments' => $allComments]);
+
+    return back();
+});
+/*
+|--------------------------------------------------------------------------
+| HAPUS KOMENTAR
+|--------------------------------------------------------------------------
+*/
+Route::delete('/comment/{nama}/{index}', function ($nama, $index) {
+
+    $comments = session()->get("comments", []);
+
+    if (isset($comments[$index])) {
+        unset($comments[$index]);
+        session()->put("comments_$nama", array_values($comments));
+    }
+
+    return back();
 });
